d2fedc6a21d65e7ec49660b426dcb28dec680d6f
Igor Rodrigues <igorjr2003@gmail.com>
2024-04-24 21:44:49 -0300

    update landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,46 @@ import NavBar from './components/nav/NavBar.js';
 import Hero from './components/hero/Hero.js';
 import Beneficios from './components/beneficios/Beneficios.js';
 
-let lastScroll = 0;
+type Position = {
+  left: number;
+  top: number;
+}
 
-// TODO - Make this handle with responsive design
+const getOffset = (e: HTMLElement) => {
+  const rect = e.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  };
+}
+
+const getDivsPositions = () => {
+  const divs = document.querySelectorAll('section');
+  const divsPositions: Position[] = [];
+  divs.forEach((div) => {
+    divsPositions.push(getOffset(div));
+  });
+  return divsPositions;
+}
+
+let index = -1;
+const divsPositions = getDivsPositions();
+
 const handleWheel = (event: React.WheelEvent<HTMLDivElement>) => {
+  console.log(divsPositions);
+
   const deltaY = event.deltaY;
-  const nDivs = document.querySelectorAll('Section').length;
-  const defaultHeight = document.querySelector('Section')?.clientHeight || 0;
-  const windowSizeY = window.innerHeight + defaultHeight * nDivs;
   if (deltaY === 0) return;
-  if (deltaY > 0 && window.scrollY >= defaultHeight * nDivs) return;
   if (deltaY < 0 && window.scrollY <= 0) return;
   if (deltaY > 0) {
-    lastScroll = window.scrollY;
-    lastScroll = (lastScroll % defaultHeight === 0) ? lastScroll : lastScroll - (lastScroll % defaultHeight);
-    lastScroll = (deltaY > 0) ? lastScroll : lastScroll - deltaY;
-    window.scroll({ top: lastScroll + defaultHeight, behavior: 'smooth' });
+    window.scroll({ top: divsPositions[index].top, behavior: 'smooth' });
+    index += 1;
   } else {
-    lastScroll = window.scrollY;
-    lastScroll = (lastScroll % defaultHeight === 0) ? lastScroll : lastScroll - (lastScroll % defaultHeight);
-    lastScroll = (deltaY > 0) ? lastScroll : lastScroll - deltaY;
-    window.scroll({ top: lastScroll - defaultHeight, behavior: 'smooth' });
+    window.scroll({ top: divsPositions[index].top, behavior: 'smooth' });
+    index -= 1;
   }
 
-};
+}
 
 function App() {
   return (

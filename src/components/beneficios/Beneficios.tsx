4eb36b156b3edf8c7f48be5d2a5a9ac590014449
igorjr-rubecube <igor.rodrigues@rubcube.com>
2024-06-06 18:04:00 -0300

    update beneficios

--- src/components/beneficios/Beneficios.tsx
+++ src/components/beneficios/Beneficios.tsx
@@ -2,48 +2,53 @@ import AccountsIcon from "./icons/AccountsIcon";
 import InfoIcon from "./icons/InfoIcon";
 import LockIcon from "./icons/LockIcon";
 import MapPinIcon from "./icons/MapPinIcon";
-import { BeneficiosSection, Title, Card, CardTitle, CardIcon, CardDiv } from "./style";
+import {
+  BeneficiosSection,
+  Title,
+  Card,
+  CardTitle,
+  CardIcon,
+  CardDiv,
+} from "./style";
 
 type CardProps = {
-    title: string;
-    icon: JSX.Element | null;
-}
+  title: string;
+  icon: JSX.Element | null;
+};
 
 const cards: CardProps[] = [
-    {
-        title: "SEGURANÇA GARANTIDA A CADA TOQUE",
-        icon:   <AccountsIcon />
-    },
-    {
-        title: "TRANSFERÊNCIAS EM QUALQUER LUGAR",
-        icon: <InfoIcon />
-    },
-    {
-        title: "ACESSO A MÚLTIPLAS CONTAS",
-        icon: <LockIcon />
-    },
-    {
-        title: "EXTRATO DETALHADO DA CONTA",
-        icon: <MapPinIcon />
-    }
+  {
+    title: "SEGURANÇA GARANTIDA A CADA TOQUE",
+    icon: <LockIcon />,
+  },
+  {
+    title: "TRANSFERÊNCIAS EM QUALQUER LUGAR",
+    icon: <MapPinIcon />,
+  },
+  {
+    title: "ACESSO A MÚLTIPLAS CONTAS",
+    icon: <AccountsIcon />,
+  },
+  {
+    title: "EXTRATO DETALHADO DA CONTA",
+    icon: <InfoIcon />,
+  },
 ];
 
-function Beneficios({id} : {readonly id: string}) {
-    return (
-        <BeneficiosSection id={id}>
-            <Title>Benefícios</Title>
-            <CardDiv>
-                {cards.map((card) => (
-                    <Card key={card.title}>
-                        <CardTitle>{card.title}</CardTitle>
-                        <CardIcon>
-                            {card.icon}
-                        </CardIcon>
-                    </Card>
-                ))}
-            </CardDiv>
-        </BeneficiosSection>
-    );
+function Beneficios({ id }: { readonly id: string }) {
+  return (
+    <BeneficiosSection id={id}>
+      <Title>Benefícios</Title>
+      <CardDiv>
+        {cards.map((card) => (
+          <Card key={card.title}>
+            <CardTitle>{card.title}</CardTitle>
+            <CardIcon>{card.icon}</CardIcon>
+          </Card>
+        ))}
+      </CardDiv>
+    </BeneficiosSection>
+  );
 }
 
 export default Beneficios;
